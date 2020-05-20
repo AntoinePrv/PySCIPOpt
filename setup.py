2b7bb234b2a266126fd4695fcfd926c0f9c0478a
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-05-20 12:50:39 -0400

    Automaticaly download cython in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, Extension
+from Cython.Build import cythonize
 import os, platform, sys, re
 
 # look for environment variable that specifies path to SCIP
@@ -41,38 +42,22 @@ if "--debug" in sys.argv:
     extra_compile_args.append('-UNDEBUG')
     sys.argv.remove("--debug")
 
-cythonize = True
-
 packagedir = os.path.join('src', 'pyscipopt')
 
 with open(os.path.join(packagedir, '__init__.py'), 'r') as initfile:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
                         initfile.read(), re.MULTILINE).group(1)
 
-try:
-    from Cython.Build import cythonize
-except ImportError:
-    if not os.path.exists(os.path.join(packagedir, 'scip.c')):
-        print('Cython is required')
-        quit(1)
-    cythonize = False
-
-if not os.path.exists(os.path.join(packagedir, 'scip.pyx')):
-    cythonize = False
-
-ext = '.pyx' if cythonize else '.c'
-
-extensions = [Extension('pyscipopt.scip', [os.path.join(packagedir, 'scip'+ext)],
-                          include_dirs=[includedir],
-                          library_dirs=[libdir],
-                          libraries=[libname],
-                          extra_compile_args = extra_compile_args,
-                          extra_link_args=extra_link_args
-                          )]
-
-if cythonize:
-    extensions = cythonize(extensions, compiler_directives = {'language_level': 3})
-#     extensions = cythonize(extensions, compiler_directives={'linetrace': True})
+extensions = [Extension(
+        'pyscipopt.scip',
+        sources=[os.path.join(packagedir, 'scip.pyx')],
+        include_dirs=[includedir],
+        library_dirs=[libdir],
+        libraries=[libname],
+        extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
+    )]
+extensions = cythonize(extensions, compiler_directives={'language_level': 3})
 
 with open('README.md') as f:
     long_description = f.read()
